b0fc194c9124664d47222ac589e005c69403bd49
Matheus Assis <matheusassissilva20@gmail.com>
2026-04-06 19:39:43 -0300

    tests: remove unnecessary class mock

--- test/infrastructure/api/routes/public_router_test.dart
+++ test/infrastructure/api/routes/public_router_test.dart
@@ -3,31 +3,13 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:test/test.dart';
 
-import 'package:sambura_core/domain/barrel.dart';
-import 'package:sambura_core/application/barrel.dart';
 import 'package:sambura_core/infrastructure/barrel.dart';
 
 class MockPackageManagerRouter extends Mock implements PackageManagerRouter {}
 
 class MockAuthController extends Mock implements AuthController {}
 
-class MockArtifactController extends Mock implements ArtifactController {}
-
-class MockBlobController extends Mock implements BlobController {}
-
 class MockSystemController extends Mock implements SystemController {}
-
-class MockMetricsController extends Mock implements MetricsController {}
-
-class MockApiKeyRepository extends Mock implements ApiKeyRepository {}
-
-class MockAccountRepository extends Mock implements AccountRepository {}
-
-class MockAuthPort extends Mock implements AuthPort {}
-
-class MockCachePort extends Mock implements CachePort {}
-
-class MockMetricsPort extends Mock implements MetricsPort {}
 
 void main() {
   setUpAll(() {
@@ -52,14 +34,10 @@ void main() {
       final mockAuthController = MockAuthController();
       final mockPackageManagerRouter = MockPackageManagerRouter();
       final mockSystemController = MockSystemController();
-      final mockMetricsController = MockMetricsController();
 
       when(
         () => mockSystemController.router,
       ).thenReturn(Router()..get('/health', (_) => Response.ok('healthy')));
-      when(
-        () => mockMetricsController.getMetrics(any()),
-      ).thenAnswer((_) async => Response.ok('metrics'));
 
       final publicRouter = PublicRouter(
         mockPackageManagerRouter,
